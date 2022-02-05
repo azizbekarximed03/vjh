81ac400f63e585e6fe1597fc997351d4003db73d
azizbekarximed03 <85345739+azizbekarximed03@users.noreply.github.com>
2022-02-05 14:03:58 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -35,11 +35,9 @@ class Config(object):
     LOG_FILE_ZZGEVC = get_config("LOG_FILE_ZZGEVC", "Log.txt")
       # because, https://t.me/c/1494623325/5603
     SHOULD_USE_BUTTONS = get_config("SHOULD_USE_BUTTONS", False)
-    STATUS = command_start = '/stats'
+    STATUS = "command_start = '/stats'
 only_for_admins = 'Sorry, this function is available only for admins. Set "admin" flag in django admin panel.'
-
 secret_admin_commands = f"⚠️ Secret Admin commands\n" \
                         f"{command_start} - bot stats"
-
 users_amount_stat = "<b>Users</b>: {user_count}\n" \
-                    "<b>24h active</b>: {active_24}"
+                    "<b>24h active</b>: {active_24}""
